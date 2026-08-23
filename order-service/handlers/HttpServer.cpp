8158b95756e1d1edc3956a96459bc7c986135f9f
ToksichniyDed <dimadyakonov9620@yandex.ru>
2026-08-23 16:23:36 +0300

    поднял сервак

--- order-service/handlers/HttpServer.cpp
+++ order-service/handlers/HttpServer.cpp
@@ -6,6 +6,7 @@
 #include <boost/beast/core.hpp>
 
 #include "HttpServer.h"
+#include <logging/Logger.h>
 
 namespace order_service::handlers {
 
@@ -13,11 +14,19 @@ namespace order_service::handlers {
     namespace http = beast::http;
     using tcp = boost::asio::ip::tcp;
 
-    HttpServer::HttpServer(order_system::models::NetworkConfiguration config): _networkConfiguration(std::move(config)), _ioContext(1), _acceptor(_ioContext, tcp::endpoint(
-                                                                                                                                                      boost::asio::ip::make_address(_networkConfiguration.address.value()), _networkConfiguration.port))  {
+    HttpServer::HttpServer(
+        order_system::models::NetworkConfiguration config) : _networkConfiguration(std::move(config)), _ioContext(1),
+                                                             _acceptor(_ioContext, tcp::endpoint(
+                                                                           boost::asio::ip::make_address(
+                                                                               _networkConfiguration.address.value()),
+                                                                           _networkConfiguration.port)) {
+        SPDLOG_LOGGER_INFO(Logger::get("HttpServer"), "Server created successfully!");
+        SPDLOG_LOGGER_INFO(Logger::get("HttpServer"),"Server address : {}", _networkConfiguration.address.value());
+        SPDLOG_LOGGER_INFO(Logger::get("HttpServer"),"Server port : {}", _networkConfiguration.port);
     }
 
     void HttpServer::run() {
+        SPDLOG_LOGGER_INFO(Logger::get("HttpServer"), "Server run successfully!");
         accept();
     }
 
