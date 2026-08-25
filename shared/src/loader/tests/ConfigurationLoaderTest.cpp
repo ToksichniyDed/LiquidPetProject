108537dbb59bad9be04e9d4134660f023e767bf1
ToksichniyDed <dimadyakonov9620@yandex.ru>
2026-08-25 20:10:09 +0300

    тесты на парсеры и конфигураторы

---
+++ shared/src/loader/tests/ConfigurationLoaderTest.cpp
@@ -0,0 +1,107 @@
+//
+// Created by DED on 23.08.2026.
+//
+
+#include <fstream>
+#include <gtest/gtest.h>
+#include "loader/ConfigurationLoader.h"
+
+namespace {
+    struct LoadSectionTestCase {
+        std::string testName;
+        std::optional<std::string> fileContent;
+        std::string sectionName;
+        bool expectSuccess;
+        std::optional<Loader::ConfigurationParseError> expectedError;
+    };
+}
+
+class LoadSectionTest : public ::testing::TestWithParam<LoadSectionTestCase> {
+protected:
+    void SetUp() override {
+        testFilePath =
+                std::filesystem::temp_directory_path()
+                / std::format(
+                    "configuration_loader_test_{}.json",
+                    GetParam().testName
+                );
+
+        if (GetParam().fileContent.has_value()) {
+            std::ofstream file(testFilePath);
+
+            ASSERT_TRUE(file.is_open());
+
+            file << *GetParam().fileContent;
+
+            ASSERT_TRUE(file.good());
+        }
+    }
+
+    void TearDown() override {
+        std::filesystem::remove(testFilePath);
+    }
+
+protected:
+    std::filesystem::path testFilePath;
+};
+
+TEST_P(LoadSectionTest, LoadSection) {
+    const auto& testCase = GetParam();
+
+    const auto result =
+            Loader::ConfigurationLoader::loadSection(
+                testFilePath,
+                testCase.sectionName
+            );
+
+    if (testCase.expectSuccess) {
+        ASSERT_TRUE(result.has_value());
+
+        return;
+    }
+
+    ASSERT_FALSE(result.has_value());
+
+    if (testCase.expectedError.has_value()) {
+        EXPECT_EQ(
+            result.error(),
+            *testCase.expectedError
+        );
+    }
+}
+
+INSTANTIATE_TEST_SUITE_P(LoadSectionTests,
+                         LoadSectionTest,
+                         ::testing::Values(
+                             LoadSectionTestCase{
+                             .testName = "LoadSectionSuccess",
+                             .fileContent = R"({"network": {"address": "0.0.0.0", "port": 8080}})",
+                             .sectionName = "network",
+                             .expectSuccess = true,
+                             .expectedError = std::nullopt
+                             },
+                             LoadSectionTestCase{
+                             .testName = "LoadSectionInvalidPathError",
+                             .fileContent = std::nullopt,
+                             .sectionName = {},
+                             .expectSuccess = false,
+                             .expectedError = Loader::ConfigurationParseError::InvalidPath
+
+                             },
+                             LoadSectionTestCase{
+                             .testName = "LoadSectionInvalidFormatError",
+                             .fileContent = R"({"network": {"address": 0.0.0.0", "port": 8080}})",
+                             .sectionName = "network",
+                             .expectSuccess = false,
+                             .expectedError = Loader::ConfigurationParseError::InvalidFormat
+
+                             },
+                             LoadSectionTestCase{
+                             .testName = "LoadSectionSectionNotFoundError",
+                             .fileContent = R"({"network": {"address": "0.0.0.0", "port": 8080}})",
+                             .sectionName = "some section",
+                             .expectSuccess = false,
+                             .expectedError = Loader::ConfigurationParseError::SectionNotFound
+                             }), [](const ::testing::TestParamInfo<LoadSectionTestCase>& info) {
+                         return info.param.testName;
+                         });
